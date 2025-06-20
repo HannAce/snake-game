9adf85e1c8fbc0700ece272afbf707eede52e0df
Hannah Fisher <hannahtfisher@gmail.com>
2025-06-20 19:11:01 +0100

    Set up snake player prefab and basic player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float m_playerSpeed;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * m_playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * m_playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * m_playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * m_playerSpeed * Time.deltaTime);
+        }
+    }
+}
