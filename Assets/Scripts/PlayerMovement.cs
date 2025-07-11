5d4904bdaa39131828dcd6a8b1addaf3f33e85e5
Hannah Fisher <hannahtfisher@gmail.com>
2025-07-11 20:31:20 +0100

    Implemented grid based movement. Stopped the player from moving back into themself. Added a visual trail (WIP)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,6 @@
 using System;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,59 +10,77 @@ namespace SnakeGame
     {
         [SerializeField] private float m_playerSpeed;
         private Vector2 m_movementDirection = Vector2.right;
+        private Vector2 m_targetPosition;
         private bool m_canMove = true;
 
         void Start()
         {
             m_canMove = true;
+            m_targetPosition = transform.position;
         }
-        
-        // void Update()
-        // {
-        //     Move();
-        // }
 
         private void FixedUpdate()
-        {
-            Move();
-        }
-
-        private void Move()
         {
             if (!m_canMove)
             {
                 return;
             }
             
-            if (Input.GetKey(KeyCode.W))
+            GetMovementInput();
+            Move();
+        }
+
+        private void GetMovementInput()
+        {
+            // If holding Up and not already going Down
+            if (Input.GetKey(KeyCode.W) && m_movementDirection.y >= 0)
             {
                 m_movementDirection = Vector2.up;
             }
-
-            if (Input.GetKey(KeyCode.S))
+            // If holding Down and not already going Up
+            else if (Input.GetKey(KeyCode.S) && m_movementDirection.y <= 0)
             {
                 m_movementDirection = Vector2.down;
             }
-
-            if (Input.GetKey(KeyCode.A))
+            // If holding Left and not already going Right
+            else if (Input.GetKey(KeyCode.A) && m_movementDirection.x <= 0)
             {
                 m_movementDirection = Vector2.left;
             }
-
-            if (Input.GetKey(KeyCode.D))
+            // If holding Right and not already going Left
+            else if (Input.GetKey(KeyCode.D) && m_movementDirection.x >= 0)
             {
                 m_movementDirection = Vector2.right;
             }
-            
-            transform.Translate(m_movementDirection * m_playerSpeed * Time.deltaTime);
-            //transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), 0.0f);
         }
 
+        private void Move()
+        {
+            // Target direction is the input for the direction player is moving
+            Vector2 targetDirection = m_movementDirection * (m_playerSpeed * Time.deltaTime);
+            
+            // Adding target direction to target position to move towards that position
+            // Alternative to transform.Translate for movement
+            m_targetPosition += targetDirection;
+            
+            // m_targetPosition above is smooth movement, like a ghost player.
+            // We will round to next int and snap to grid position before moving actual player
+            Vector2 gridPosition = new Vector2(Mathf.Round(m_targetPosition.x), Mathf.Round(m_targetPosition.y));
+            transform.position = gridPosition;
+        }
+        
         public void StopMovement()
         {
             m_movementDirection = Vector2.zero;
             m_canMove = false;
-            
+        }
+
+         
+        private void OnDrawGizmos()
+        {
+            // Draws a gizmo to represent the ghost player/target position
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(m_targetPosition, 0.5f);
         }
     }
 }
