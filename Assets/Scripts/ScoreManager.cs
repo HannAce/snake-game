fc0836b8f3983ad599ff46c61e32515a90a8df13
Hannah Fisher <hannahtfisher@gmail.com>
2025-07-23 22:58:59 +0100

    Added saving and displaying high score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace SnakeGame
@@ -6,13 +7,34 @@ namespace SnakeGame
     {
         [SerializeField] private ScoreUI m_scoreUI;
         private int m_score;
+        private int m_highScore;
 
         public int Score => m_score;
-        
+        public int HighScore => m_highScore;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            m_highScore = PlayerPrefs.GetInt("HighScore", 0);
+        }
+
         public void AddScore(int score)
         {
             m_score += score;
             m_scoreUI.UpdateScoreUI();
+            TrySetHighScore();
+        }
+
+        public void TrySetHighScore()
+        {
+            if (m_highScore >= m_score)
+            {
+                return;
+            }
+
+            m_highScore = m_score;
+            m_scoreUI.UpdateHighScoreUI();
+            PlayerPrefs.SetInt("HighScore", m_highScore);
         }
     }
 }
--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -1,23 +1,31 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SnakeGame
 {
     public class ScoreUI : MonoBehaviourSingleton<ScoreUI>
     {
-        [SerializeField] private TMP_Text m_ScoreText;
+        [SerializeField] private TMP_Text m_scoreText;
+        [SerializeField] private TMP_Text m_highScoreText;
         private ScoreManager m_scoreManager;
 
         void Start()
         {
             m_scoreManager = ScoreManager.Instance;
 
-            m_ScoreText.text = "Score: 0";
+            m_scoreText.text = "Score: 0";
+            m_highScoreText.text = $"High Score: {m_scoreManager.HighScore.ToString()}";
         }
 
         public void UpdateScoreUI()
         {
-            m_ScoreText.text = $"Score: {m_scoreManager.Score}";
+            m_scoreText.text = $"Score: {m_scoreManager.Score}";
+        }
+
+        public void UpdateHighScoreUI()
+        {
+            m_highScoreText.text = $"High Score: {m_scoreManager.HighScore.ToString()}";
         }
     }
 }
